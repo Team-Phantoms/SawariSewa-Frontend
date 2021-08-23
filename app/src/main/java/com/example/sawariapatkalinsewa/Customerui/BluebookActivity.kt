b6d80d9ea99aa9aa0d83fe6200fd3d9f5628e503
Insane Kill <ravipaudel59@gmail.com>
2021-08-23 20:54:29 +0545

    Bluebook activity make

---
+++ app/src/main/java/com/example/sawariapatkalinsewa/Customerui/BluebookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sawariapatkalinsewa.Customerui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sawariapatkalinsewa.R
+
+class BluebookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluebook)
+    }
+}
