550e0585ea9f72bd7e994bf327da7196d510bd10
Insane Kill <ravipaudel59@gmail.com>
2021-08-31 21:15:53 +0545

    changes in forget password activiy and xml file

--- app/src/main/java/com/example/sawariapatkalinsewa/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/sawariapatkalinsewa/ForgetPasswordActivity.kt
@@ -2,10 +2,24 @@ package com.example.sawariapatkalinsewa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ForgetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var newPassword: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var btnSearch: Button
+    private lateinit var reset: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+
+        username=findViewById(R.id.username)
+        newPassword=findViewById(R.id.newPassword)
+        confirmPassword=findViewById(R.id.confirmPassword)
+        btnSearch=findViewById(R.id.btnSearch)
+        reset=findViewById(R.id.reset)
     }
 }
